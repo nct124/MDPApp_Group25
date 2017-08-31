d327362c737d4d9a884ff4b95065c4d154caf7b5
NCT-PC\n <nct_124@hotmail.com>
2017-08-31 18:10:53 +0800

    Initial commit

---
+++ app/src/test/java/mdp/cz3004/ntu/com/mdpapp_group25/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package mdp.cz3004.ntu.com.mdpapp_group25;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
